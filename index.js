56f3698a7a3547a1f1bac926badee54b45db5002
bonveeee <bonochieng@gmail.com>
2022-06-08 22:31:27 +0300

    commit tree 0246bf54bb8f2caf8c48d6e0074954d6c2a14078

---
+++ index.js
@@ -0,0 +1,18 @@
+function receivesAFunction(spy){
+    return spy()
+}
+
+function returnsANamedFunction (){
+        var fn = function(){          
+}
+return fn;
+}
+function returnsAnAnonymousFunction (){
+    return function (){
+        return "name"
+    }
+}
+
+
+
+
